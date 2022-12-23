09943e376b704423b8732ef7d9c6eaef7e66dc87
muhmand <107528622+muhmand@users.noreply.github.com>
2022-12-22 20:00:09 -0500

    Palindrome Method Solved

--- src/string_practice_with_boot/Palindrome.java
+++ src/string_practice_with_boot/Palindrome.java
@@ -2,36 +2,12 @@ package string_practice_with_boot;
 
 public class Palindrome {
 
-
-    public static void main(String[] args) {
-
-        int x = 2567121;
-        String intToString = ""+x;
-
-        String intTo = String.valueOf(x);
-        String temp = "";
-        for (int i = intTo.length()-1; i >= 0 ; i--) {
-            temp += "" +intTo.charAt(i);
-        }
-        if(intTo.equals(temp)){
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
-
-
-        System.out.println(" = " + isPalindrome(431134));
-
-
-    }
-
         public static int isPalindrome(int num){
             int x = 0;
             int value = 1;
         if(num < 0){
             return x;
         }
-
         while (num > 0){            //
             value = num % 10;       // 121 % 10 == 1 give us the last one (1)
             x += value;             // storing the last num to x   (x = 1)
@@ -43,9 +19,29 @@ public class Palindrome {
         }
 
 
+                            // 121
+
+    public static void palindrome(Integer x){
+      String str = String.valueOf(x) ;
+      String temp = "";
+        for (int i = str.length()-1; i > -1; i--) {        // 7 8 9 7
+            temp += "" + str.charAt(i);                 //   0 1 2 3 4
+        }
+        if (str.equals(temp)){
+            System.out.println("IS PALINDROME");
+        } else {
+            System.out.println("IN NOT PALINDROME");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        palindrome(787);
 
 
 
+    }
 
 
 
